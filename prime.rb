a1e5483f115cd8ce1bb971796011fa8bda011b90
CCroteau28 <ccroteau28@gmail.com>
2021-03-02 20:30:54 -0700

    commit tree add385dae342a7c065da078bc7d0b5d84e25857f

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(numbers)
+   if numbers < 2
+      return false
+   end
+   (2..(numbers - 1)).each do |i|
+    return false if numbers % i == 0
+   end
+  return true
+end
